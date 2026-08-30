57b067e7fdd3643808857310d959642d6a8a5719
RafaelJ13 <rjanuario@ipcbcampus.pt>
2026-08-30 20:12:53 +0100

    feat: create users table migration

---
+++ app/Database/Migrations/2026-08-30-183915_CreateAccountsTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
